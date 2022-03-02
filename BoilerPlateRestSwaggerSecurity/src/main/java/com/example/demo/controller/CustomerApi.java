62981ceb4184ab36f0fbd6c892982c9ae0116a16
adamantium1987 <29163549+adamantium1987@users.noreply.github.com>
2022-03-02 17:00:45 -0600

    Reverteed API and ApiController for Employee, Customer, Manager Admin

--- BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/controller/CustomerApi.java
+++ BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/controller/CustomerApi.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
 
 @Tag(name = "customer", description = "the customer API")
 @RequestMapping("/api/v1/customer")
--- BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/controller/EmployeeApiController.java
+++ BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/controller/EmployeeApiController.java
@@ -9,39 +9,5 @@ import java.util.Collection;
 
 @RestController
 @SecurityRequirement(name="BoilerPlateAPI")
-public class EmployeeApiController implements UserApi {
-    @Override
-    public ResponseEntity<User> findById(long id) throws Exception {
-        return null;
-    }
-
-    @Override
-    public Collection<User> findUsers() {
-        return null;
-    }
-
-    @Override
-    public User updateUser(String id, User user) {
-        return null;
-    }
-
-    @Override
-    public User patchUser(String id, User user) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<User> postUser(User body) throws Exception {
-        return null;
-    }
-
-    @Override
-    public User getUserHeader() {
-        return null;
-    }
-
-    @Override
-    public long deleteUser(long id) throws Exception {
-        return 0;
-    }
+public class EmployeeApiController {
 }
