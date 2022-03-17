05a8d06080e692102928f6a4e1b5d2158c908f24
adamantium1987 <29163549+adamantium1987@users.noreply.github.com>
2022-03-17 10:40:24 -0500

    Updated Readme, and Removed user creation code from main.

--- BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/DemoApplication.java
+++ BoilerPlateRestSwaggerSecurity/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.auth.UserService;
-import com.example.demo.model.Role;
-import com.example.demo.model.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,28 +9,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-
-/*    @Bean
-    CommandLineRunner run(UserService userService) {
-        return args -> {
-            userService.saveRole(new Role(null, "ROLE_CUSTOMER"));
-            userService.saveRole(new Role(null, "ROLE_EMPLOYEE"));
-            userService.saveRole(new Role(null, "ROLE_MANAGER"));
-            userService.saveRole(new Role(null, "ROLE_ADMINISTRATOR"));
-            userService.saveRole(new Role(null, "ROLE_ADMINISTRATOR_READONLY"));
-
-            userService.saveUser(new User(null, "Administrator", "admin", "1234", new ArrayList<>()));
-            userService.saveUser(new User(null, "AdministratorReadOnly", "adminro", "1234", new ArrayList<>()));
-            userService.saveUser(new User(null, "Manager", "manager", "1234", new ArrayList<>()));
-            userService.saveUser(new User(null, "Employee", "employee", "1234", new ArrayList<>()));
-            userService.saveUser(new User(null, "Customer", "customer", "1234", new ArrayList<>()));
-
-            userService.addRoleToUser("customer", "ROLE_CUSTOMER");
-            userService.addRoleToUser("employee", "ROLE_EMPLOYEE");
-            userService.addRoleToUser("manager", "ROLE_MANAGER");
-            userService.addRoleToUser("adminro", "ROLE_ADMINISTRATOR_READONLY");
-            userService.addRoleToUser("admin", "ROLE_ADMINISTRATOR");
-        };
-    }*/
 }
--- README.md
+++ README.md
@@ -9,22 +9,19 @@ navigate to localhost:8080/swagger-ui.html to view the API documentation
 H2 Database Credentials
 H2 URL: /console
 Username: sa
-Password: password
+Password:  // password for database is empty
 
 
 Application Credentials (User are setup via data.sql passwords are hased using Bcrypt)
-Admin
-Username: admin
-Password: 1234
-Manager
-Username: manager
-Password: 1234
-Employee
-Username: employee
-Password: 1234
-Customer
-Username: customer
-Password: 1234
+ROLE                    | Username     | Hashed Password                                              | Plain textpassword
+Administrator			| admin        | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+AdministratorReadOnly'	| adminrro     | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+Manager				    | manager      | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+ManagerReadOnly	    	| managerro    | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+Employee				| employee     | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+EmployeeReadOnly		| employeero   | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+Customer				| customer     | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
+CustomerReadOnly		| customerro   | $2a$12$gluwkDnuiEawB5g/ohUGDeV3IeGl3oUxPQmmWVkocZlp360MG91TC | 1234
 
 To-Do:
 Improve readme file
